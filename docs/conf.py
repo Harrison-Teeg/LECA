94950e0c9dcedcccc6e00c769b0a546c9b5e98ed
Harrison Martin <h_mart05@uni-muenster.de>
2023-10-08 17:41:39 +0200

    troubleshooting rtd build

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ import os
 import sys
 autodoc_mock_imports = ["hdbscan", "GPy", "GPyOpt", 'mapie', 'ipywidgets']
 sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath(os.path.join('..', 'LECA')))
-#sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'LECA')))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -86,4 +86,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
